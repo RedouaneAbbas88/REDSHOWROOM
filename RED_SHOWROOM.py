c4e5f08c11e9002fb46c96a552c16acbb4214bf6
Redouane Abbas <redouaneabbas88@gmail.com>
2025-09-25 11:18:59 +0100

    Mise à jour

--- RED_SHOWROOM.py
+++ RED_SHOWROOM.py
@@ -265,7 +265,7 @@ elif tab_choice == "📦 État Stock":
         if not df_ventes.empty:
             ventes_group = df_ventes.groupby("Produit")["Quantité"].sum().reset_index()
             stock_reel = stock_reel.merge(ventes_group, on="Produit", how="left", suffixes=('', '_vendu'))
-            stock_reel['Quantité_vendu'] = stock_reel['Quantité_vendu"].fillna(0)
+            stock_reel['Quantité_vendu'] = stock_reel['Quantité_vendu'].fillna(0)
             stock_reel['Stock restant'] = stock_reel['Quantité'] - stock_reel['Quantité_vendu']
         else:
             stock_reel['Stock restant'] = stock_reel['Quantité']
